5691b8819571ff871b82d051a0a66d61fee25945
Mauricio <mauricioaizaga@gmail.com>
2012-06-12 22:43:50 -0500

    Reparando el repositorio

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-$url = "http://mauricioaizaga.com/LaOtraEskina/";/*Cambiar este link dependiendo del servidor en que se encuentre alojado*/
+//$url = "http://mauricioaizaga.com/LaOtraEskina/";/*Cambiar este link dependiendo del servidor en que se encuentre alojado*/
+$url = "http://laotraeskinatab.herokuapp.com/";/*Cambiar este link dependiendo del servidor en que se encuentre alojado*/
+
 //$url="";
 $titulo = "La Otra Eskina &gt; ".$musico;
 $page="http://www.facebook.com/LaOtraEskinaPereira?sk=app_161408067264807";
---
+++ git.php
@@ -0,0 +1,9 @@
+<?php
+$git = "git status";
+$id=exec($git,$salida);
+//foreach($salida as $line) {
+//    echo "$line<br>";
+//}
+
+
+?>
